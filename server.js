126c9d0d6414e738029deba415b2e2d6ffc38d2d
Sarah Fogarty <sarahefogarty@gmail.com>
2017-02-27 20:00:51 -0800

    Commit 2

--- server.js
+++ server.js
@@ -3,28 +3,24 @@ var app = express();
 var server = app.listen(8000)
 var path = require('path');
 
-//Body Parser
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-//Session
 var session = require('express-session')
 app.use(session({
 	secret:'keyboard cat',
 	resave: false,
-	saveUninitialized: true 
+	saveUninitialized: true
 }))
 
 app.use(express.static(path.join(__dirname,'./client')));
 
-//Mongoose
 require('./server/config/mongoose.js');
 
-//HTTP Routes
 require('./server/config/routes.js')(app);
 
 
 
-// app.listen(8000, function(){
-// 	console.log('8000');
-// })
+app.listen(8000, function(){
+	console.log('listening on 8000');
+})
